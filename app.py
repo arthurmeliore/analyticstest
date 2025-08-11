071d62fbbc6cd3bbc142da49bf3f4a73f0164b26
arthurmeliore <arthur.kakande@gsccnetwork.org>
2025-08-11 09:27:16 +0000

    added g analytics

--- app.py
+++ app.py
@@ -1,5 +1,21 @@
 import streamlit as st
 
+# Inject Google Analytics tag
+st.markdown(
+    """
+    <!-- Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-4BGMRKV2LJ"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-4BGMRKV2LJ');
+    </script>
+    <!-- Google tag (gtag.js) -->
+    """,
+    unsafe_allow_html=True
+)
+
 st.title("Simple Analytics App")
 st.write("Welcome to the Simple Analytics App!")
 
