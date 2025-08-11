12ebc6b2a06f71c9612d04ceee6e6d8a9ae6d784
arthurmeliore <arthur.kakande@gsccnetwork.org>
2025-08-11 09:43:39 +0000

    added html file

--- app.py
+++ app.py
@@ -1,20 +1,10 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
-# Inject Google Analytics using a hidden HTML component
-components.html(
-    """
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-4BGMRKV2LJ"></script>
-    <script>
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-      gtag('config', 'G-4BGMRKV2LJ');
-    </script>
-    """,
-    height=0,
-    width=0
-)
+# Include Google Analytics tracking code
+with open("google_analytics.html", "r") as f:
+    html_code = f.read()
+    components.html(html_code, height=0)
 
 st.title("Simple Analytics App")
 st.write("Welcome to the Simple Analytics App!")
